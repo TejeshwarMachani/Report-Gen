688cecfb5aa69c41489f1873680f4ef51a950d54
Machani Tejeshwar <130921398+TejeshwarMachani@users.noreply.github.com>
2026-09-15 16:16:36 +0000

    Before: change the name in frontend from insight navigator to report gen and improve the front end to a pro ...

---
+++ isolate/assets/skeleton-B1D--NeF.js
@@ -0,0 +1,1 @@
+import{j as o}from"./framer-motion-B-Zw8xvZ.js";import{c as s}from"./logo-DjDmoyHU.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
